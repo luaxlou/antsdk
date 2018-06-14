5373153eb7195dd9fa3ce5ca62d31d4a4c9cab7c
Joy <gionna@qq.com>
2018-06-14 16:40:11 +0800

    fix AlipaySystemOauthToken 返回参数类型错误

--- api/util/AlipaySystemOauthTokenResponse.go
+++ api/util/AlipaySystemOauthTokenResponse.go
@@ -1,15 +1,15 @@
 package util
 
 import (
-  "github.com/gionna/antsdk/api"
+	"github.com/gionna/antsdk/api"
 )
 
 type AlipaySystemOauthTokenResponse struct {
-  api.AlipayResponse
-  AlipayUserId  string `json:"alipay_user_id"`
-  UserId        string `json:"user_id"`
-  AccessToken   string `json:"access_token"`
-  ExpiresIn     string `json:"expires_in"`
-  RefreshToken  string `json:"refresh_token"`
-  ReExpiresIn   string `json:"re_expires_in"`
+	api.AlipayResponse
+	AlipayUserId string `json:"alipay_user_id"`
+	UserId       string `json:"user_id"`
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	ReExpiresIn  int    `json:"re_expires_in"`
 }
